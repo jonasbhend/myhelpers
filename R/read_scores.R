e60c8f0eb88ae91e2c0fbaa32d7306bfb84a94f7
Jonas Bhend <jonas.bhend@gmx.ch>
2016-01-11 13:36:34 +0100

    Include skill scores against reference forecast

--- R/read_scores.R
+++ R/read_scores.R
@@ -2,8 +2,9 @@
 #'
 #' Read skill scores to data frame
 #'
+#' @param models character vector of the names of forecasting systems
 #' @param scores character vector of skill scores to be read
-#' @param indices character vector of variable names
+#' @param indexes character vector of variable names
 #' @param methods character vector of bias correction methods
 #' @param initmonths character vector of initialization months
 #' @param grids character vector of grids
@@ -11,14 +12,19 @@
 #' @param ccrs logical should recalibrated indices be used?
 #' @param detrends logical should detrended series be used?
 #' @param leads index of lead times
+#' @param reference name of reference model against which the forecast skill
+#' is evaluated. If set to \code{NULL} (the default), forecasts are
+#' verified against a climatological forecast.
 #' @param dpath head of directory tree
 #'
 #' @keywords util
 #' @export
-read_scores <- function(scores=c('EnsCorr', 'FairRpss'), indices='tas',
+read_scores <- function(models='ecmwf-system4',
+                        scores=c('EnsCorr', 'FairRpss'), indexes='tas',
                         methods='none', initmonths="05", grids="global2",
                         granularities="seasonal", ccrs=c(FALSE, TRUE),
                         detrends=FALSE, leads=2,
+                        reference=NULL,
                         dpath="/store/msclim/bhendj/EUPORIAS"){
   stopifnot(file.exists(dpath))
   skill.long <- list()
@@ -34,7 +40,8 @@ read_scores <- function(scores=c('EnsCorr', 'FairRpss'), indices='tas',
     lat <- rep(nc$dim[[lolaname[2]]]$vals, each=nc$dim[[lolaname[1]]]$len)
     nc_close(nc)
 
-    skill <- expand.grid(index=indices, method=methods, initmon=initmonths,
+    skill <- expand.grid(model=models, index=indexes, method=methods,
+                         initmon=initmonths,
                          score=scores, granularity=granularities, ccr=ccrs,
                          detrend=detrends, lead=leads)
     skill$grid <- grid
@@ -42,11 +49,17 @@ read_scores <- function(scores=c('EnsCorr', 'FairRpss'), indices='tas',
     skill <- cbind(skill, matrix(NA, nrow(skill), length(lsm)))
 
     ## convert to filename and filepath
-    filepaths <- paste(dpath, 'skill_scores', grid, skill$granularity, skill$index, sep='/')
-    filenames <- paste0('^', skill$index, c('', '_detrend')[skill$detrend*1 + 1],
-                        c('', '_CCR')[skill$ccr*1 + 1], '_',
-                        skill$method, '_.*initmon', skill$initmon, '.nc$')
-
+    if (is.null(reference)){
+      filepaths <- paste(dpath, 'skill_scores', grid, skill$granularity, skill$index, sep='/')
+      filenames <- paste0('^', skill$index, c('', '_detrend')[skill$detrend*1 + 1],
+                          c('', '_CCR')[skill$ccr*1 + 1], '_',
+                          skill$method, '_', skill$model, '_.*initmon', skill$initmon, '.nc$')
+    } else {
+      filepaths <- paste(dpath, 'skill_against_reference', grid, skill$granularity, skill$index, sep='/')
+      filenames <- paste0('^', skill$index, c('', '_detrend')[skill$detrend*1 + 1],
+                          c('', '_CCR')[skill$ccr*1 + 1], '_',
+                          skill$method, '_', skill$model, '-ref-', reference, '_.*initmon', skill$initmon, '.nc$')
+    }
     ## read in scores
     for (i in 1:nrow(skill)){
       infile <- list.files(filepaths[i], filenames[i], full.names=TRUE)
@@ -58,7 +71,7 @@ read_scores <- function(scores=c('EnsCorr', 'FairRpss'), indices='tas',
             lolaname <- sapply(nc$var[[as.character(skill$score[i])]]$dim, function(x) x$name)[1:2]
             lon2 <- rep(nc$dim[[lolaname[1]]]$vals, nc$dim[[lolaname[2]]]$len)
             lat2 <- rep(nc$dim[[lolaname[2]]]$vals, each=nc$dim[[lolaname[1]]]$len)
-            stopifnot(lon2 == lon, lat2 == lat)
+            stopifnot(round(lon2,3) == round(lon,3), round(lat2,3) == round(lat,3))
             if (skill$score[i] %in% c('EnsRmse', 'FairCrps', 'FairSprErr')){
               skill[i,ncol(skill) - rev(seq(lsm) - 1)] <- pmax(c(dtmp), 0)
             } else {
@@ -76,7 +89,7 @@ read_scores <- function(scores=c('EnsCorr', 'FairRpss'), indices='tas',
     skill.long[[grid]][['lsm']] <- c(lsm)[skill.long[[grid]][['gridID']]]
     skill.long[[grid]][['lon']] <- c(lon)[skill.long[[grid]][['gridID']]]
     skill.long[[grid]][['lat']] <- c(lat)[skill.long[[grid]][['gridID']]]
-
+    skill.long[[grid]][['reference']] <- reference
   } # end of loop on grids
 
   ## output single data frame if only one grid is read
